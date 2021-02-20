33150fa8592c2dea6570d8bcdc549f045e1976ce
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2021-02-20 22:35:04 +0100

    Fix bug with buttons

--- ButtonManager.cpp
+++ ButtonManager.cpp
@@ -37,7 +37,7 @@ namespace {
   }
 
 
-  byte triggerBlockButtons(ButtonBlockAddr blockAddr, byte &state, ButtonManager::Callback callback1, ButtonManager::Callback callback2,
+  void triggerBlockButtons(ButtonBlockAddr blockAddr, byte &state, ButtonManager::Callback callback1, ButtonManager::Callback callback2,
                            ButtonManager::Callback callback3, ButtonManager::Callback callback4, ButtonManager::Callback callback5,
                            ButtonManager::Callback callback6, ButtonManager::Callback callback7, ButtonManager::Callback callback8
   ) {
@@ -51,9 +51,8 @@ namespace {
 
     digitalWrite(PIN_CLOCK_BTN_17, LOW);
     delay(1);
-    digitalWrite(PIN_CLOCK_BTN_17, HIGH);
 
-    return state ^ (
+    state = state ^ (
         triggerButton(state, PIN_DATA_1, 0b00000001, callback1) |
         triggerButton(state, PIN_DATA_2, 0b00000010, callback2) |
         triggerButton(state, PIN_DATA_3, 0b00000100, callback3) |
@@ -63,6 +62,7 @@ namespace {
         triggerButton(state, PIN_DATA_7, 0b01000000, callback7) |
         triggerButton(state, PIN_DATA_8, 0b10000000, callback8)
     );
+    digitalWrite(PIN_CLOCK_BTN_17, HIGH);
   }
 
 }
@@ -84,32 +84,32 @@ void ButtonManager::update() {
     pinMode(pin, INPUT);
   }
 
-  _states[0] = triggerBlockButtons(U524, _states[0], _callbacks[TIME_4], _callbacks[TIME_4T],
+  triggerBlockButtons(U524, _states[0], _callbacks[TIME_4], _callbacks[TIME_4T],
                                    _callbacks[TIME_8], _callbacks[TIME_8T],
                                    _callbacks[TIME_16], _callbacks[TIME_16T],
                                    _callbacks[TIME_32], _callbacks[TIME_32T]
   );
 
-  _states[1] = triggerBlockButtons(U221, _states[1], _callbacks[BANK_1_8],
+  triggerBlockButtons(U221, _states[1], _callbacks[BANK_1_8],
                                    _callbacks[BANK_9_16], _callbacks[BANK_17_24],
                                    _callbacks[BANK_25_32], _callbacks[MODE_SEQ_PITCH],
                                    _callbacks[MODE_SEQ_CC], _callbacks[MODE_MIDI_CC],
                                    _callbacks[TIME_DIV]
   );
-  _states[2] = triggerBlockButtons(U220, _states[2], _callbacks[UP], _callbacks[DOWN],
+  triggerBlockButtons(U220, _states[2], _callbacks[UP], _callbacks[DOWN],
                                    _callbacks[LEFT], _callbacks[RIGHT],
                                    _callbacks[EDIT_STORE], _callbacks[PROGRAM_STD],
                                    _callbacks[GLOBAL_HUI], _callbacks[PROG_CHANGE]
   );
-  _states[3] = triggerBlockButtons(U223, _states[3], _callbacks[PREV], _callbacks[STOP],
+  triggerBlockButtons(U223, _states[3], _callbacks[PREV], _callbacks[STOP],
                                    _callbacks[PLAY], _callbacks[RECORD], _callbacks[NEXT], _callbacks[SHIFT],
                                    _callbacks[TEMPO], _callbacks[ENTER]
   );
-  _states[4] = triggerBlockButtons(U222, _states[4], _callbacks[OCT_DOWN], _callbacks[OCT_UP],
+  triggerBlockButtons(U222, _states[4], _callbacks[OCT_DOWN], _callbacks[OCT_UP],
                                    _callbacks[PLAY], _callbacks[RECORD], _callbacks[NEXT], _callbacks[SHIFT],
                                    _callbacks[ARP_MODE], _callbacks[SEQ_SELECT]
   );
-  _states[5] = triggerBlockButtons(U272, _states[5], _callbacks[ON_OFF], _callbacks[LATCH],
+  triggerBlockButtons(U272, _states[5], _callbacks[ON_OFF], _callbacks[LATCH],
                                    _callbacks[NOTE_REPEAT], _callbacks[PAD_BANK_A],
                                    _callbacks[PAD_BANK_B], _callbacks[PAD_BANK_C], _callbacks[PAD_BANK_D],
                                    _callbacks[FULL_LEVEL]
